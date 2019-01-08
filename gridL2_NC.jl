90861368bd1cda5997220c9162e2bedb242dd7a6
Christian Frankenberg <cfranken@tofu.gps.caltech.edu>
2019-01-08 09:38:43 -0800

    Added first commit

---
+++ gridL2_NC.jl
@@ -0,0 +1,279 @@
+#!/home/cfranken//julia
+
+# Argument Parser
+using ArgParse
+using Base
+# NetCDF tools for reading and writing
+using NCDatasets
+# Basic statistics
+using Statistics
+# File search and completion
+using Glob
+# JSON files
+import JSON
+# Parallel computing
+using Distributed, SharedArrays
+# Profiler
+using Profile
+
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+    	"--inDict"
+            help = "JSON dictionary file to use"
+            arg_type = String
+            default = "/home/cfranken/tropomi.json"
+        "--gridDict"
+            help = "JSON dictionary file to use for gridding variables"
+            arg_type = String
+            default = "/home/cfranken/tropomi_grid.json"
+        "--outFile", "-o"
+            help = "output filename (default OCO2_SIF_map.nc)"
+            arg_type = String
+            default = "OCO2_SIF_map.nc"
+        "--latMin"
+            help = "Lower latitude bound"
+            arg_type = Float32
+            default = -90.0f0
+        "--latMax"
+            help = "Upper latitude bound"
+            arg_type = Float32
+            default = 90.0f0
+        "--lonMin"
+            help = "Lower longitude bound"
+            arg_type = Float32
+            default = -180.0f0
+        "--lonMax"
+            help = "Upper longitude bound"
+            arg_type = Float32
+            default = 180.0f0
+        "--dLat"
+            help = "latitude resolution"
+            arg_type = Float32
+            default = 1.0f0
+        "--dLon"
+            help = "longitude resolution"
+            arg_type = Float32
+            default = 1.0f0
+        "--folder"
+            help = "folder with L2 NC files"
+            arg_type = String
+            default = "/net/fluo/data2/projects/TROPOMI/nc_ungridded/"
+    end
+    return parse_args(s)
+end
+
+# This splits up the entire region into one grid set
+function divLine!(lat1,lon1,lat2,lon2,n, points,j )
+    dLat = (lat2-lat1)/(2*n)
+    dLon = (lon2-lon1)/(2*n)
+	startLat = lat1+dLat
+	startLon = lon1+dLon
+	for i in 1:n
+		#println(startLat+2*(i-1)*dLat, " ", startLon+2*(i-1)*dLon)
+		#weights[(iLat-minLat+1), (iLon-minLon+1)]+=1
+		points[j,i,1] = startLat+2*(i-1)*dLat
+		points[j,i,2] = startLon+2*(i-1)*dLon
+	end
+end
+
+# For first run (2 baselines)
+function divLine2!(lat1,lon1,lat2,lon2,n, lats, lons)
+    dLat = (lat2-lat1)/(2*n)
+    dLon = (lon2-lon1)/(2*n)
+	startLat = lat1+dLat
+	startLon = lon1+dLon
+	for i in 1:n
+	    lats[i] = startLat+2*(i-1)*dLat
+	    lons[i] = startLon+2*(i-1)*dLon
+	end
+end
+
+# Divide each polygon into multiple points
+function getPoints!(points, vert_lat, vert_lon, n,lats_0, lons_0,lats_1, lons_1 )
+	# Get reference points for two lines at the extremes:
+	# println(vert_lat)
+	divLine2!(vert_lat[1],vert_lon[1],vert_lat[2],vert_lon[2],n,lats_0, lons_0)
+    divLine2!(vert_lat[4],vert_lon[4],vert_lat[3],vert_lon[3],n,lats_1, lons_1)
+    for i in 1:n
+         divLine!(lats_0[i], lons_0[i] ,lats_1[i], lons_1[i],n, points,i )
+    end
+end
+
+# Test for how fast we can compute point in distributed mode (not yet used)
+function compPoints!(lat,lon,n)
+	dim = size(lat)
+	lats_0 = zeros(n)
+	lons_0 = zeros(n)
+	lats_1 = zeros(n)
+	lons_1 = zeros(n)
+	a = SharedArray{Float32}((dim[1],n,n,2))
+	@distributed for i in 1:dim[1]
+        po = getPoints!(a[i,:,:,:],lat[i,:],lon[i,:],n,lats_0, lons_0,lats_1, lons_1 )
+    end
+    return a
+end
+
+function favg_all!(arr,lat,lon,inp,s,s2,n, latMin, latMax, lonMin, lonMax, nLat, nLon, points)
+	dim = size(arr)
+	#println(dim)
+	# Predefine some arrays to reduce allocations
+	ix = zeros(Int32,n^2)
+	iy = zeros(Int32,n^2)
+	lats_0 = zeros(n)
+	lons_0 = zeros(n)
+	lats_1 = zeros(n)
+	lons_1 = zeros(n)
+	iLon = floor.(Int32,lon)
+	iLat = floor.(Int32,lat)
+	minLat = minimum(Int32,floor.(lat), dims=2)
+	maxLat = maximum(Int32,floor.(lat), dims=2)
+	minLon = minimum(Int32,floor.(lon), dims=2)
+	maxLon = maximum(Int32,floor.(lon), dims=2)
+	distLon = maxLon-minLon
+	# How many individual grid cells might actually be there:
+	global dimLat = maxLat-minLat
+	global dimLon = maxLon-minLon
+	fac = n^2
+    for i in 1:s
+		#println(i, " ", dimLat[i], " ", dimLon[i])
+		# Take it easy if all corners already fall into one grid box:
+		if (dimLat[i]==1) & (dimLon[i]==1)
+			for z in 1:s2
+	                arr[iLon[i,1],iLat[i,1],z] +=  fac*inp[i,z]
+	        end
+		# if not, compute appropriate weights
+		elseif (distLon[i])<n
+	        getPoints!(points,lat[i,:],lon[i,:],n,lats_0, lons_0,lats_1, lons_1 )
+	        ix[:] = floor.(Int32,points[:,:,1][:])
+	        iy[:] = floor.(Int32,points[:,:,2][:])
+			#iy[iy .> dim[1]].=dim[1]
+			#iy[iy .< 1].=1
+
+	        for j in eachindex(ix)
+	            for z in 1:s2
+	                arr[iy[j],ix[j],z] +=  inp[i,z]
+	                #println(arr[ix[j],iy[j],z])
+	            end
+	        end
+		#else
+			#println("Warning ", iLat[i], " ",iLon[i], " ", maxLon[i]-minLon[i])
+		end
+    end
+end
+
+function main()
+
+	#addprocs()
+    # Parse command line arguments
+    ar = parse_commandline()
+
+    # Just lazy (too cumbersome in code as often used variables here)
+    latMax = ar["latMax"]
+    latMin = ar["latMin"]
+    lonMax = ar["lonMax"]
+    lonMin = ar["lonMin"]
+    dLat = ar["dLat"]
+    dLon = ar["dLon"]
+    eps = dLat/100
+
+    # Define spatial grid:
+    lat = collect(latMin+dLat/2.:dLat:latMax-dLat/2.0+eps)
+    lon = collect(lonMin+dLon/2.:dLon:lonMax-dLon/2.0+eps)
+	println("Output file dimension (lon/lat):")
+    println(length(lon)," ", length(lat))
+    # Create output file:
+    dsOut = Dataset("testGrid.nc","c")
+    defDim(dsOut,"lon",length(lon))
+	defDim(dsOut,"lat",length(lat))
+	dsLat = defVar(dsOut,"lat",Float32,("lat",))
+	dsLon = defVar(dsOut,"lon",Float32,("lon",))
+	dsLat[:]=lat
+	dsLon[:]=lon
+
+
+	# Define a global attribute
+	dsOut.attrib["title"] = "Awesome gridded file"
+
+    # Define gridded variables:
+    n=zeros(Float32,(length(lat),length(lon)))
+    SIF = zeros(Float32,(length(lat),length(lon)))
+    # Parse JSON files as dictionary
+    d2 = JSON.parsefile(ar["inDict"])
+    dGrid = JSON.parsefile(ar["gridDict"])
+	NCDict= Dict{String, NCDatasets.CFVariable}()
+	println("Creating NC datasets in output:")
+	for (key, value) in dGrid
+		println(key)
+		NCDict[key] = defVar(dsOut,key,Float32,("lon","lat"),deflatelevel=4, fillvalue=-999)
+	end
+
+	#dSIF = defVar(dsOut,"sif",Float32,("lon","lat"),deflatelevel=4, fillvalue=-999)
+	dN = defVar(dsOut,"n",Float32,("lon","lat"),deflatelevel=4, fillvalue=-999)
+    # Define data array
+    mat_data= zeros(Float32,(length(lon),length(lat),1+length(dGrid)))
+
+	# Still hard-coded here, can be changed:
+    nGrid = 10
+    points = zeros(Float32,(nGrid,nGrid,2))
+    # Find files to be processed
+    files = glob("TROPO_SIF_2018-0[7]-??_*.nc", ar["folder"])
+
+	#global indices = zeros(Int32,(nGrid,nGrid,2))
+	global weights = zeros(Int32,(nGrid,nGrid))
+
+    # Loop through all files
+    for a in files
+        # Read NC file
+        fin = Dataset(a)
+        # Check lat/lon first to see what data to read in
+        lat_in = fin[d2["lat"]].var[:]
+        lon_in = fin[d2["lon"]].var[:]
+        lat_in_ = fin[d2["lat_bnd"]].var[:]
+        lon_in_ = fin[d2["lon_bnd"]].var[:]
+        # Find all indices within lat/lon bounds:
+		minLat = minimum(lat_in_, dims=2)
+		maxLat = maximum(lat_in_, dims=2)
+		minLon = minimum(lon_in_, dims=2)
+		maxLon = maximum(lon_in_, dims=2)
+
+		#idx = findall((lat_in.>latMin).&(lat_in.<latMax).&(lon_in.>lonMin).&(lon_in.<lonMax))
+		#println("IDX ", size(idx), " ", size(minLat[:,1]), " ", size(lat_in))
+		idx = findall((minLat[:,1].>latMin).&(maxLat[:,1].<latMax).&(minLon[:,1].>lonMin).&(maxLon[:,1].<lonMax))
+		#println("IDX ", size(idx))
+		# Read data only for non-empty indices
+        if length(idx) > 0
+            mat_in =  zeros(Float32,(length(lat_in),length(dGrid)+1))
+			dim = size(mat_in)
+            # Read in all entries defined in JSON file:
+			co = 1
+            for (key, value) in dGrid
+            	mat_in[:,co]=fin[value].var[:]
+				co += 1
+            end
+            mat_in[:,end].=1
+            iLat_ = ((lat_in_[idx,:].-latMin)/(latMax-latMin)*length(lat)).+1
+            iLon_ = ((lon_in_[idx,:].-lonMin)/(lonMax-lonMin)*length(lon)).+1
+			@time  favg_all!(mat_data, iLat_,iLon_,mat_in[idx,:],length(idx),dim[2],nGrid, latMin, latMax, lonMin,lonMax, length(lat), length(lon), points )
+            println("Read ", a, " ", length(idx))
+        else
+            println("Read ", a, " ", length(idx))
+        end
+        close(fin)
+    end
+	# Filter all data, set averages
+	dims = size(mat_data)
+	println("Averaging final product...")
+
+	dN[:,:]=mat_data[:,:,end]./nGrid^2
+	co = 1
+	for (key, value) in dGrid
+		NCDict[key][:,:]=round.(mat_data[:,:,co]./mat_data[:,:,end],sigdigits=5)
+		NCDict[key][dN.<1].=-999
+		co += 1
+	end
+    close(dsOut)
+end
+
+main()
